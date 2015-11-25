cffa3eee601ba9ff0f2acb3f6067b8551c357745
Scott Postlethwaite <chinnygan@hotmail.co.uk>
2015-11-25 14:50:40 +0000

    Final commit

--- ArduinoCode/FinalIntegration/FinalIntegration.ino
+++ ArduinoCode/FinalIntegration/FinalIntegration.ino
@@ -16,6 +16,7 @@
 #include <Adafruit_ILI9341.h>
 #include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h>
+#include <pitches.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
@@ -54,11 +55,6 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(19, LEDStrip, NEO_GRB + NEO_KHZ800);
 //RFID variables
 boolean scanned_card = false;
 
-int serNum[5];
-int test[][5] = {
-  {
-    131,222,128,0,221      }
-};
 
 bool access = false;
 
@@ -67,10 +63,17 @@ String bt_buffer;
 String last_bt = "";
 boolean available_bt = false;
 
+//Order Status
+String order_status = "unknown";
 
+//dank
+int memes = 0;
+
+//timeouts
+//unsigned long timeout = millis();
 
 void setup(){
-
+  
   Serial.begin(9600);               // starts serial monitor at 9600bps
   SPI.begin();                      // starts SPI
   rfid.init();                      // initialises RFID library
@@ -87,17 +90,39 @@ void setup(){
   strip.begin();
   strip.show();
 
-  colorWipe(strip.Color(0, 0, 0), 10); //off init
-
-
   pinMode(speaker, OUTPUT);
   tone(speaker,1000);
   delay(100);
   noTone(speaker);
+ 
 
 }
 
 void loop(){
+  
+    // RFID reading
+  if(rfid.isCard()){
+    if(rfid.readCardSerial()){
+      String serNumStr = "";
+      for(int i=0; i<5; i++){
+        serNumStr = serNumStr + String(rfid.serNum[i], HEX) + " ";              
+      }
+      bluetooth.print("c," + serNumStr);
+
+      scanned_card = true;
+
+      screen_drawn = 8;
+
+      tone(speaker, 800);
+      delay(120);
+      tone(speaker, 1000);
+      delay(40);
+      noTone(speaker);
+
+    }       
+  }
+
+  rfid.halt();
 
   //Display switch
   switch(display_state) {
@@ -105,22 +130,93 @@ void loop(){
 
     loginDisplay();
 
+    if (digitalRead(BTN0)) { 
+      tone(speaker,NOTE_C6);
+      delay(100);
+      noTone(speaker);
+      if(memes < 3) {
+        colorWipe((0, 0, 255), 1);
+        memes++;
+      }
+      else if (memes == 6) {
+        colorWipe((0, 0, 255), 1);
+        memes++;
+      }
+      else {
+        colorWipe((0, 0, 0), 1);
+        memes = 0;
+      }
+    }
+    if (digitalRead(BTN1)) {
+      tone(speaker,NOTE_F6);
+      delay(100);
+      noTone(speaker);
+      if(memes == 3) {
+      colorWipe((0, 0, 255), 1);
+        memes++;
+      }
+       else {
+       colorWipe((0, 0, 0), 1);
+         memes = 0;
+       }
+    }
+
+    if (digitalRead(BTN2)) {
+      tone(speaker,NOTE_DS6);
+      delay(100);
+      noTone(speaker);
+      if(memes == 4) {
+      colorWipe((0, 0, 255), 1);
+        memes++;
+      }
+       else {
+       colorWipe((0, 0, 0), 1);
+         memes = 0;
+       }
+    }
+
+    if (digitalRead(BTN3)) {
+      tone(speaker,NOTE_AS5);
+      delay(100);
+      noTone(speaker);
+      if(memes == 5) {
+      colorWipe((0, 0, 255), 1);
+        memes++;
+      } else {
+       colorWipe((0, 0, 0), 1);
+         memes = 0;
+      }
+    }
+
+
+    if(memes == 7) {
+      memes = 0;
+      darude();
+    }
+
+
+
     if(digitalRead(BTN0) && digitalRead(BTN3)) { //Shorcut to main menu for debugging purposes
       scanned_card = false;
       display_state = 1;
       screen_drawn = 0;
     }
 
+
+
     if(scanned_card) {
       if(available_bt) {
 
-        if (read_bt() == "access1") { //Card is good
+        String access = read_bt();
+
+        if (access == "access1") { //Card is good
           scanned_card = false;
           display_state = 1;
           screen_drawn = 0;
         }
         else {
-          tft.println("Sorry, no access");
+          tft.setCursor(75,210);
+          tft.println("Sorry," + access);
           scanned_card = false;
           delay(1000);
           screen_drawn = 0;
@@ -136,11 +232,33 @@ void loop(){
     //If buttons pressed move to corresponding screen
     // Read buttons and send to bluetooth
     if (digitalRead(BTN0)) { //Bill
+      tone(speaker,500);
+      delay(100);
+      noTone(speaker);
       display_state = 2;
       screen_drawn = 0;
     }
-    if (digitalRead(BTN1)) { //Bill
+    if (digitalRead(BTN1)) { //Leave
+      tone(speaker,1500);
+      delay(100);
+      noTone(speaker);
       display_state = 3;
+      screen_drawn = 0;
+    }
+
+    if (digitalRead(BTN2)) { //Food Status
+      tone(speaker,2000);
+      delay(100);
+      noTone(speaker);
+      display_state = 4;
+      screen_drawn = 0;
+    }
+
+    if (digitalRead(BTN3)) { //Call server
+      tone(speaker,1000);
+      delay(100);
+      noTone(speaker);
+      display_state = 5 ;
       screen_drawn = 0;
     }
 
@@ -151,9 +269,17 @@ void loop(){
   case 2: //Bill called
     billDisplay();
     break;
-    
+
   case 3: //Leave Table
     leaveDisplay();
+    break;
+
+  case 4: //Food Status
+    foodDisplay();
+    break;
+
+  case 5: //Call Server
+    waiterDisplay();
     break;
 
   default:
@@ -166,13 +292,13 @@ void loop(){
 
 
   // RFID reading
-  if(rfid.isCard() && !scanned_card){
+  if(rfid.isCard()){
     if(rfid.readCardSerial()){
       String serNumStr = "";
       for(int i=0; i<5; i++){
-        serNumStr = serNumStr + String(rfid.serNum[i]) + " ";              
+        serNumStr = serNumStr + String(rfid.serNum[i], HEX) + " ";              
       }
-      bluetooth.print("CARD_ID," + serNumStr);
+      bluetooth.print("c," + serNumStr);
 
       scanned_card = true;
 
@@ -241,6 +367,7 @@ void loginDisplay(){
 
   switch(screen_drawn) {
   case 0: // Initialise
+    colorWipe(strip.Color(0, 0, 0), 25);
     tft.fillScreen(ILI9341_BLACK);
     tft.setTextSize(2);
     tft.setCursor(126,113); // centred text
@@ -303,7 +430,7 @@ void loginDisplay(){
 
 
   case 8: //Please wait
-    tft.setCursor(126,200); // centred text
+    tft.setCursor(100,200); // centred text
     tft.println("Please wait");
     screen_drawn = 1;
     break;
@@ -315,7 +442,7 @@ void homeDisplay(){
   case 0: //Setup
 
     //LEDs blue
-    colorWipe(strip.Color(0, 0, 255), 10);
+    colorWipe(strip.Color(0, 0, 255), 25);
 
 
     tft.fillRect(0,0,160,120,ILI9341_LIGHTGREY);
@@ -350,7 +477,7 @@ void billDisplay(){
   case 0: //Setup
 
     //LEDs yellow
-    colorWipe(strip.Color(255, 255, 0), 10);
+    colorWipe(strip.Color(255, 255, 0), 25);
 
     tft.fillRect(160,0,160,120,ILI9341_RED);
 
@@ -372,7 +499,7 @@ void billDisplay(){
     break;
 
   case 2: //Exit state
-    colorWipe(strip.Color(0, 255, 0), 10);
+    colorWipe(strip.Color(0, 255, 0), 25);
 
     tft.fillRect(160,0,160,120,ILI9341_GREEN);
 
@@ -380,7 +507,7 @@ void billDisplay(){
     tft.setCursor(165,60);
     tft.println("Bill on way");
 
-    delay(2000);
+    delay(1500);
     screen_drawn = 0;
     display_state = 1; //Main menu
     break;
@@ -396,15 +523,15 @@ void waiterDisplay(){
   case 0: //Setup
 
     //LEDs yellow
-    colorWipe(strip.Color(255, 255, 0), 10);
+    colorWipe(strip.Color(255, 255, 0), 25);
 
-    tft.fillRect(160,0,160,120,ILI9341_RED);
+    tft.fillRect(0,0,160,120,ILI9341_RED);
 
     tft.setTextSize(2);
-    tft.setCursor(165,60);
-    tft.println("Calling bill");
+    tft.setCursor(5,60);
+    tft.println("Calling server");
 
-    bluetooth.print("request_bill");
+    bluetooth.print("call_waiter");
 
     screen_drawn = 1;
     break;
@@ -418,15 +545,15 @@ void waiterDisplay(){
     break;
 
   case 2: //Exit state
-    colorWipe(strip.Color(0, 255, 0), 10);
+    colorWipe(strip.Color(0, 255, 0), 25);
 
-    tft.fillRect(160,0,160,120,ILI9341_GREEN);
+    tft.fillRect(0,0,160,120,ILI9341_GREEN);
 
     tft.setTextSize(2);
-    tft.setCursor(165,60);
-    tft.println("Bill on way");
+    tft.setCursor(5,60);
+    tft.println("Server on way");
 
-    delay(2000);
+    delay(1500);
     screen_drawn = 0;
     display_state = 1; //Main menu
     break;
@@ -442,12 +569,12 @@ void leaveDisplay(){
   case 0: //Setup
 
     //LEDs yellow
-    colorWipe(strip.Color(255, 255, 0), 10);
+    colorWipe(strip.Color(255, 255, 0), 25);
 
     tft.fillRect(160,120,160,120,ILI9341_RED);
 
     tft.setTextSize(2);
-    tft.setCursor(165,60);
+    tft.setCursor(165,180);
     tft.println("Hang on!");
 
     bluetooth.print("leave_table");
@@ -464,17 +591,17 @@ void leaveDisplay(){
     break;
 
   case 2: //Exit state
-    colorWipe(strip.Color(0, 255, 0), 10);
+    colorWipe(strip.Color(0, 255, 0), 25);
 
-    tft.fillRect(160,0,160,120,ILI9341_GREEN);
+    tft.fillRect(160,120,160,120,ILI9341_GREEN);
 
     tft.setTextSize(2);
-    tft.setCursor(165,60);
-    tft.println("Bill on way");
+    tft.setCursor(165,180);
+    tft.println("Goodbye");
 
     delay(2000);
     screen_drawn = 0;
-    display_state = 1; //Main menu
+    display_state = 0; //login screen
     break;
 
   default:
@@ -488,47 +615,173 @@ void foodDisplay(){
   case 0: //Setup
 
     //LEDs yellow
-    colorWipe(strip.Color(255, 255, 0), 10);
+    colorWipe(strip.Color(255, 255, 0), 25);
 
-    tft.fillRect(160,0,160,120,ILI9341_RED);
+    tft.fillRect(0,120,160,120,ILI9341_RED);
 
     tft.setTextSize(2);
-    tft.setCursor(165,60);
-    tft.println("Calling bill");
+    tft.setCursor(5,180);
+    tft.println("Status check");
 
-    bluetooth.print("request_bill");
+    bluetooth.print("order_status");
+    
+//    timeout = millis();
 
     screen_drawn = 1;
+    
     break;
 
   case 1: //Loop
     if(available_bt) {
-      if( read_bt() == "ok") {
-        screen_drawn = 2;
-      }
+      screen_drawn = 2;
     }
-    break;
+    
+//    if (timeout - millis() > 10000) {
+//      screen_drawn = 3;
+//    }
+//    break;
 
   case 2: //Exit state
-    colorWipe(strip.Color(0, 255, 0), 10);
+      colorWipe(strip.Color(0, 255, 0), 25);
 
-    tft.fillRect(160,0,160,120,ILI9341_GREEN);
+    tft.fillRect(0,120,160,120,ILI9341_GREEN);
+
+    order_status = read_bt();
 
     tft.setTextSize(2);
-    tft.setCursor(165,60);
-    tft.println("Bill on way");
+    tft.setCursor(5,175);
+    tft.println(order_status);
 
-    delay(2000);
+    delay(1500);
+//    tft.fillRect(0,120,160,120,ILI9341_LIGHTGREY);
+//    tft.setCursor(5,180);
+//    tft.println("Food status");
     screen_drawn = 0;
     display_state = 1; //Main menu
     break;
+    
+//  case 3: //Timeout state
+//    
+//    colorWipe(strip.Color(255, 0, 0), 25);
+//
+//    tft.fillRect(0,120,160,120,ILI9341_BLUE);
+//
+//    tft.setTextSize(2);
+//    tft.setCursor(5,175);
+//    tft.println("Try again");
+//
+//    delay(1500);
+//    tft.fillRect(0,120,160,120,ILI9341_LIGHTGREY);
+//    tft.setCursor(5,180);
+//    tft.println("Food status");
+//    screen_drawn = 1;
+//    display_state = 1; //Main menu
+//    break;
 
   default:
     break;
 
   }
+}
+
+
+void darude() {
+  
+  rainbowCycle(0);
+
+  int melody[] = {
+    NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6,
+    NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6,
+    NOTE_F6, NOTE_F6, NOTE_F6, NOTE_F6, NOTE_F6,
+    NOTE_DS6, NOTE_DS6, NOTE_DS6, NOTE_DS6, NOTE_DS6,
+    
+    NOTE_AS5, NOTE_AS5, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6,
+    NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6,
+    NOTE_DS6, NOTE_DS6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6,
+    
+    NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6,
+//    NOTE_DS6, NOTE_DS6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6,
+  };
+
+  // note durations: 4 = quarter note, 8 = eighth note, etc.:
+  int noteDurations[] = {
+    8, 8, 8, 8, 4,
+    8, 8, 8, 8, 8, 8, 4,
+    8, 8, 8, 8, 4,
+    8, 8, 8, 8, 4,
+    
+    8, 8, 8, 8, 8, 8, 4,
+    8, 8, 8, 8, 8, 8, 4,
+    8, 8, 8, 8, 8, 8, 4,
+    
+    8, 8, 8, 8, 8, 8, 4,
+//    8, 8, 8, 8, 8, 8, 4,
+    
+  };
+  for (int thisNote = 0; thisNote < sizeof(melody); thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(speaker, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(speaker);
+  } 
+  
+  colorWipe((0,0,0),25);
 
 }
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+
+
 
 
 
